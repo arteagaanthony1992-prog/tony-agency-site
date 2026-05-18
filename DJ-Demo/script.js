bc5fe6d64e66cdb794fe9ea053cc53f33a107ded
Tony <arteagaanthony1992@gmail.com>
2026-05-17 21:54:49 -0700

    sync and update

--- DJ-Demo/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("bookingForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  const form = e.target;
-  form.innerHTML = "<h2>✅ Booking Request Sent!</h2><p>We’ll contact you within 24 hours.</p>";
-});
